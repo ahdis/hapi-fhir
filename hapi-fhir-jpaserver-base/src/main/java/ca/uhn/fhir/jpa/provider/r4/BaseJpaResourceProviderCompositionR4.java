f270c5af191a8b64c91c3252392ffb19cae426d0
Oliver Egger <oliver.egger@ahdis.ch>
2019-05-29 20:57:09 +0200

    $document operation

--- hapi-fhir-jpaserver-base/src/main/java/ca/uhn/fhir/jpa/provider/r4/BaseJpaResourceProviderCompositionR4.java
+++ hapi-fhir-jpaserver-base/src/main/java/ca/uhn/fhir/jpa/provider/r4/BaseJpaResourceProviderCompositionR4.java
@@ -1,5 +1,21 @@
 package ca.uhn.fhir.jpa.provider.r4;
 
+import java.util.Date;
+import java.util.List;
+
+import org.hl7.fhir.instance.model.api.IBaseBundle;
+import org.hl7.fhir.instance.model.api.IBaseResource;
+import org.hl7.fhir.r4.model.BooleanType;
+import org.hl7.fhir.r4.model.Bundle;
+import org.hl7.fhir.r4.model.Composition;
+import org.hl7.fhir.r4.model.IdType;
+import org.hl7.fhir.r4.model.Meta;
+import org.hl7.fhir.r4.model.Resource;
+import org.hl7.fhir.r4.model.UnsignedIntType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+import ca.uhn.fhir.jpa.dao.IFhirResourceDao;
 import ca.uhn.fhir.jpa.dao.IFhirResourceDaoComposition;
 import ca.uhn.fhir.jpa.util.JpaConstants;
 import ca.uhn.fhir.model.api.annotation.Description;
@@ -13,12 +29,7 @@ import ca.uhn.fhir.rest.api.SortSpec;
 import ca.uhn.fhir.rest.api.server.IBundleProvider;
 import ca.uhn.fhir.rest.api.server.RequestDetails;
 import ca.uhn.fhir.rest.param.DateRangeParam;
-import org.hl7.fhir.instance.model.api.IBaseBundle;
-import org.hl7.fhir.instance.model.api.IBaseResource;
-import org.hl7.fhir.r4.model.*;
-
-import java.util.List;
-
+import ca.uhn.fhir.rest.server.exceptions.ResourceNotFoundException;
 
 /*
  * #%L
@@ -42,6 +53,10 @@ import java.util.List;
 
 public class BaseJpaResourceProviderCompositionR4 extends JpaResourceProviderR4<Composition> {
 
+	@Autowired
+	@Qualifier("myBundleDaoR4")
+	protected IFhirResourceDao<Bundle> myBundleDao;
+
 	/**
 	 * Composition/123/$document
 	 *
@@ -55,6 +70,10 @@ public class BaseJpaResourceProviderCompositionR4 extends JpaResourceProviderR4<
 
 			@IdParam
 			IdType theId,
+
+			@Description(formalDefinition="Whether to store the document at the bundle end-point (/Bundle) or not once it is generated. Value = true or false (default is for the server to decide)")
+			@OperationParam(name = "persist")
+			BooleanType persist,
 
 			@Description(formalDefinition="Results from this method are returned across multiple pages. This parameter controls the size of those pages.")
 			@OperationParam(name = Constants.PARAM_COUNT)
@@ -74,13 +93,31 @@ public class BaseJpaResourceProviderCompositionR4 extends JpaResourceProviderR4<
 		startRequest(theServletRequest);
 		try {
 			IBundleProvider bundleProvider = ((IFhirResourceDaoComposition<Composition>) getDao()).getDocumentForComposition(theServletRequest, theId, theCount, theLastUpdated, theSortSpec, theRequestDetails);
-			List<IBaseResource> resourceList = bundleProvider.getResources(0, bundleProvider.size());
-
-			boolean foundCompositionResource = false;
-			Bundle bundle = new Bundle().setType(Bundle.BundleType.DOCUMENT);
-			for (IBaseResource resource : resourceList) {
-				bundle.addEntry(new Bundle.BundleEntryComponent().setResource((Resource) resource));
+			if (bundleProvider.size()==0) {
+				throw new ResourceNotFoundException(theId);
 			}
+			List<IBaseResource> resourceList = bundleProvider.getResources(0, bundleProvider.size());
+			Bundle bundle = new Bundle().setType(Bundle.BundleType.DOCUMENT);
+			String bundleFullUrlBase = theServletRequest.getRequestURL().toString();
+			bundleFullUrlBase = bundleFullUrlBase.substring(0,bundleFullUrlBase.lastIndexOf("/Composition")+1);
+			
+			for (IBaseResource resource : resourceList) {
+				if (resource!=null && resource.getIdElement()!=null) {
+					Bundle.BundleEntryComponent bundleEntryComponent = new Bundle.BundleEntryComponent();
+					bundleEntryComponent.setFullUrl(bundleFullUrlBase + resource.getIdElement().getResourceType()+"/"+resource.getIdElement().getIdPart());
+					bundleEntryComponent.setResource((Resource) resource);
+					bundle.addEntry(bundleEntryComponent);
+				} 
+			}
+			
+			if (persist!=null && persist.booleanValue() && myBundleDao!=null) {
+				myBundleDao.create(bundle);
+			} else {
+				Meta meta = new Meta();
+				meta.setLastUpdated(new Date());
+				bundle.setMeta(meta);							
+			}
+			
 			return bundle;
 		} finally {
 			endRequest(theServletRequest);
